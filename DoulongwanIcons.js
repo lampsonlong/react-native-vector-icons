60dadf2827b38ddbf3f322d8313ea11890fcd719
jojo <394590166@qq.com>
2019-05-16 14:48:04 +0800

    增加DLW图标字体

---
+++ DoulongwanIcons.js
@@ -0,0 +1,18 @@
+/**
+ * DoulongwanIcons icon set component.
+ * Usage: <DlwIcons name="icon-name" size={20} color="#4F8EF7" />
+ */
+
+import createIconSet from './lib/create-icon-set';
+import glyphMap from './glyphmaps/DoulongwanIcons.json';
+
+const iconSet = createIconSet(glyphMap, 'DlwIcons', 'DoulongwanIcons.ttf');
+
+export default iconSet;
+
+export const Button = iconSet.Button;
+export const TabBarItem = iconSet.TabBarItem;
+export const TabBarItemIOS = iconSet.TabBarItemIOS;
+export const ToolbarAndroid = iconSet.ToolbarAndroid;
+export const getImageSource = iconSet.getImageSource;
+
